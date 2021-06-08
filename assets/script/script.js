1bda8b0c84be6e8d8787e907f372e86bcc0c88c0
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-06-08 23:39:21 +0100

    Add basic gameboard html structure

---
+++ assets/script/script.js
@@ -0,0 +1,17 @@
+//events on loading: 
+//focus on the first avatar
+//who you want to play against?
+//load instructions as a separate module
+
+//game setup:
+//close/toggle the hide help
+//select avatar 
+//select who goes first
+
+//game functions: 
+//
+//dice throw - random number
+//move pin
+//move when on a "ladder" field
+//move when on a "green" field
+//
