791249ea2f194b3824e1e5af678a10c74b9e7152
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-06-15 00:31:01 +0100

    Add js to generate the board

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,52 @@
-//events on loading: 
-//wait for DOM to load
-//add event listeners to .button: help, close, and dice 
-//load instructions as a separate module localStorage
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//             if (localStorage.getItem('instrDisplayed') == true) {
-//                 instructions.style.display = "none";
-//             }
-//         }
-
 function hide() {
     let instructions = document.getElementById("instructions");
     instructions.style.display !== "none" ? instructions.style.display = "none" : instructions.style.display = "block";
     localStorage.setItem('instructionsShown', 'true');
+
 }
 document.addEventListener("DOMContentLoaded", function() {
-        if (localStorage.getItem('instructionsShown')) {
-            instructions.style.display = "none";
-        }
-    })
-    //         }
+    if (localStorage.getItem('instructionsShown')) {
+        instructions.style.display = "none";
+    }
+    createGameBoard();
+})
+
+//generate gameboard option 1 by row
+
+// function createGameBoard() {
+//     let gameBoard = document.getElementById("game-board");
+//     for (let r = 0; r < 5; r++) {
+//         let row = document.createElement("div");
+//         row.className = 'board-row';
+//         for (let i = 0; i < 5; i++) {
+//             let field = document.createElement("div");
+//             field.className = 'field';
+//             row.appendChild(field);
+//             field.innerHTML = i;
+//         }
+//         gameBoard.appendChild(row);
+
+//     }
+
+// }
+
+//generate gameboard option 2 by div
+
+function createGameBoard() {
+    let gameBoard = document.getElementById("game-board");
+    change = false;
+    for (let i = 0; i < 25; i++) {
+        let field = document.createElement("div");
+        field.className = 'field';
+        change = i % 5 === 0 || i === 0;
+        field.innerHTML = i;
+        field.id = i;
+        gameBoard.appendChild(field);
+        change = false;
+    }
+
+
+}
 
 //game setup:
 //close/toggle help button to show instructions
