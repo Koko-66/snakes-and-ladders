a05edb4065dc5d1f3e135fa0ff61c2a356a518f1
Koko-66 <52763151+Koko-66@users.noreply.github.com>
2021-06-18 07:35:34 +0100

    Add style to avatars on click; change ai in-board avatar background to white.

--- assets/script/script.js
+++ assets/script/script.js
@@ -33,6 +33,8 @@ for (let avatar of avatars) {
 
 //throw dice to choose who goes first
 
+
+
 //generates random number between 1 and 6
 function diceThrow() {
     return Math.floor(Math.random() * 6) + 1;
@@ -49,7 +51,6 @@ dice.addEventListener("click", function() {
 })
 
 // generates gameboard by row and then field
-
 function createGameBoard() {
     let gameBoard = document.getElementById('game-board');
     for (let r = 1; r < 6; ++r) {
@@ -63,16 +64,16 @@ function createGameBoard() {
         }
         gameBoard.appendChild(row);
     }
-
-    //adds numbers to the board and creates objects to push to board array for tracking
-    let i = 25; //descending order to start game from the bottom of the board
+    //adds numbers to the board and creates objects to push to board array for tracking 
+    //descending order to start game from the bottom of the board
+    let i = 25;
     let fields = document.getElementsByClassName("field");
     let board = []
     for (field of fields) {
         let position = { position: i }
         board.push(position);
-
-        if (i === 1) { // changes first field to "start" and last to "end" and adds separate divs for ai and player avatars with id's
+        // changes first field to "start" and last to "end" and adds separate divs for ai and player avatars with id's
+        if (i === 1) {
             field.innerHTML = `Start<div id ="player-f1" class="player"></div> <div id ="ai-f1" class="ai"><img src="assets/images/avatar_red.png" alt="red avatar"></div>`;
         } else if (i === 25) {
             field.innerHTML = `End<div id ="player-f25" class="player"></div> <div id ="ai-f25" class="ai">`;
@@ -85,14 +86,12 @@ function createGameBoard() {
     console.log(board);
 }
 
-
-
-
-//add 
-
+//to generate snakes and ladders automatically: genearate 2 (easy) 4 (medium) 5 (difficult) random numbers; 
+//when number matches i place snake
+//generate random numbers for ladders
+//when number matches i check: if no snake in field and field above (might need to redo the field ids to "r1f3" for easier checks, would be fine since the avatars will move by avatar divs ids
 
 //game functions: 
-//
 
 //move avatar
 //move when on a "ladder" field
